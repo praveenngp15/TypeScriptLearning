6597f155f4018b0c5eb5c7b4292dd354ec30f0c5
PraveenKumar Natarajan <praveenkumar.natarajan@flatironssolutions.com>
2020-04-30 14:39:31 +0530

    Third Party Library

--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -1,15 +1,6 @@
+import * as _ from 'lodash';
 
-interface HasName  {
-    firstName:string,
-    lastName: string
-}
 
-interface HasAddress  {
-    address:string
-}
-
-type Player = (HasName & HasAddress) | null;
-
-const player = {firstName: "Magic", lastName:"Johnson", address:"Staples"};
-
-console.log(player);
+const colors =["Red","Green"]
+const firstColor = _.first(colors)
+console.log(firstColor)
