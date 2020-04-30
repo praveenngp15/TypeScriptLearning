d4e76dd5a6b39dd9d59bf880db6d7abdce978288
PraveenKumar Natarajan <praveenkumar.natarajan@flatironssolutions.com>
2020-04-30 13:01:33 +0530

    Typescript2 Nullable

--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -4,12 +4,15 @@ enum PlayerPosition{
     Center
 }
 
-type Player=[string,PlayerPosition]
+interface Player{
+    name:string,
+    playerPos:PlayerPosition;
+}
 
-let kobe:Player=["Kobe",PlayerPosition.Guard]
-let kevin:Player=["Kevin",PlayerPosition.Center]
-let john:Player = ["John",PlayerPosition.Forward]
+let kevin: Player | null ={
+    name : "Kevin",
+    playerPos : PlayerPosition.Guard
+}
 
-let players: Player[] =[kobe,kevin,john]
 
-console.log(players)
+kevin = null;
